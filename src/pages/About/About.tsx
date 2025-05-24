c48eb12df345ca5cfeb85bcced3fbb0c07067a3c
hellwail <superwow0887@gmail.com>
2025-05-25 02:50:08 +0300

    i zustand 5.0.5

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,3 +1,3 @@
 export const About = () => {
-    return <h1>ABOUT</h1>
+    return <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur maxime nesciunt atque neque perferendis sed sunt voluptates quaerat, unde, aliquid ipsam saepe illo fugit dolorum, dolor beatae assumenda magnam est.</h1>
 }
